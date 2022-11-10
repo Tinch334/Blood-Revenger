e7ee71b20425acf6fff0f895f84caac3991186a4
maph <juani.bertoni73@gmail.com>
2022-11-10 18:41:52 -0300

    ya nodarisa

--- Assets/Scripts/Misc/ProjectileHandler.cs
+++ Assets/Scripts/Misc/ProjectileHandler.cs
@@ -8,11 +8,14 @@ public class ProjectileHandler : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
+      
         if (collision.transform.CompareTag("Enemy"))
         {
             collision.transform.GetComponent<EnemyHealthHandler>().dealDamage(projectileDamage);
+            Debug.Log("colision proyectil");
         }
-
+        //gameObject.GetComponent<CapsuleCollider>().SetActive(false);
         Destroy(gameObject);
+        //gameObject.SetActive(false);
     }
 }
---
+++ Assets/Scripts/Misc/TomahawkHandler.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TomahawkHandler : MonoBehaviour
+{
+    [SerializeField] private int tomahawkDamage;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("colision proyectil");
+        if (other.transform.CompareTag("Enemy"))
+        {
+            other.transform.GetComponent<EnemyHealthHandler>().dealDamage(tomahawkDamage);
+        }
+        Destroy(this.gameObject);
+    }
+}
